74b30e666fe41864561adb641bb59d3361b06446
ishqair shukair <ishukair@depaul.edu>
2025-05-11 21:52:54 -0500

    Item class represents an item that gives a +2 bonus to Craft.

---
+++ Assignment2_PeterSavinos_IshqairShukair/src/main/java/oop350/model/Shield.java
@@ -0,0 +1,6 @@
+package oop350.model;
+
+public final class Shield extends Item {
+    @Override public int getCraft() { return 2; }
+    @Override public String toString() { return "Shield (+2 CRAFT)"; }
+}
